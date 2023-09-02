0ac4c33fa039d56cc21b7a0b860cfbb8e9f95b2a
avideb24 <debavi24@gmail.com>
2023-09-02 13:10:08 +0600

    all done except sort the videos by views

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const loadCetagory = async () => {
 
     const categoryContainer = document.getElementById('category-container');
 
+    const allData = data.data[0].category_id;
+
+    getCategory(allData);
+
     data.data.forEach((category) => {
         const btn = document.createElement('button');
         btn.innerHTML = `
@@ -16,27 +20,47 @@ const loadCetagory = async () => {
 
 };
 
+function postDate(totalSec) {
 
-const getCategory = async(categoryId) => {
+    const days = Math.floor(totalSec / (60 * 60 * 24));
+    const remainingSeconds = totalSec % (60 * 60 * 24);
+    const hours = Math.floor(remainingSeconds / (60 * 60));
+    const remainingSecondsAfterHours = remainingSeconds % (60 * 60);
+    const minutes = Math.floor(remainingSecondsAfterHours / 60);
+
+    const daysText = days > 0 ? `${days}days${days > 1 ? '' : ''}, ` : '';
+    const hoursText = hours > 0 ? `${hours}hrs${hours > 1 ? '' : ''}, ` : '';
+    const minutesText = minutes > 0 ? `${minutes}min${minutes > 1 ? '' : ''}` : '';
+
+    return (` ${hoursText} ${minutesText} ago`)
+}
+
+
+const getCategory = async (categoryId) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const data = await response.json();
-    // console.log(data.data)
-    // const arrayData = data.data;
-    // if(arrayData.length === 0){
-    //     console.log('got it')
-    //     const noContent = document.getElementById('no-content');
-    //     noContent.classList.remove('hidden')
-    // }
-    
+    console.log(data.data);
+
+    const arrayData = data.data;
+
+
+    if (arrayData.length === 0) {
+        // console.log(arrayData);
+        const noContent = document.getElementById("no-content");
+        noContent.classList.remove("hidden");
+    }
+
     const videoContainer = document.getElementById('video-container');
     videoContainer.innerHTML = '';
 
     data.data.forEach((video) => {
+        // console.log(video);
         const div = document.createElement('div');
         div.innerHTML = `
             <div>
                 <div class="relative h-44 bg-black rounded-xl">
                     <img src=${video.thumbnail} class="object-fill object-center w-full h-full rounded-xl " alt="">
+                    <p class="rounded absolute block text-xs bg-black p-1 px-2 text-white z-20 bottom-2 right-3">${video.others.posted_date ? postDate(video.others.posted_date) : ''}</p>
                 </div>
                 <div class="flex gap-3 mt-5">
                     <div class="author-image w-8 h-8 rounded-full">
@@ -54,18 +78,12 @@ const getCategory = async(categoryId) => {
             </div>
         `;
         videoContainer.appendChild(div);
-        
-    })
-    
 
+    })
 };
 
 
-
 loadCetagory();
-
-
-
 
 
 
